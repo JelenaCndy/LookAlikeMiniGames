7aede47e27d59066c94b4766adc4b2df7952f75c
JelenaCndy <142896286+JelenaCndy@users.noreply.github.com>
2026-04-21 12:57:52 +0200

    v8

--- TemplateData/UnitySetup.js
+++ TemplateData/UnitySetup.js
@@ -3,11 +3,11 @@ function unityShowBanner(msg, type) {
     }
     
     var buildUrl = "Build";
-    var loaderUrl = buildUrl + "/v7.loader.js";
+    var loaderUrl = buildUrl + "/v8.loader.js";
     var config = {
-        dataUrl: buildUrl + "/v7.data",
-        frameworkUrl: buildUrl + "/v7.framework.js",
-        codeUrl: buildUrl + "/v7.wasm",
+        dataUrl: buildUrl + "/v8.data",
+        frameworkUrl: buildUrl + "/v8.framework.js",
+        codeUrl: buildUrl + "/v8.wasm",
         streamingAssetsUrl: "StreamingAssets",
         companyName: "DefaultCompany",
         productName: "MiniGames",
